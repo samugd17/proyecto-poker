169dc82e2ee0b06c803745fc56a811c908e528ec
Samugd17 <samugd17@gmail.com>
2023-05-19 16:04:04 +0100

    Se añade estructura mejorada

--- cards.py
+++ cards.py
@@ -5,10 +5,24 @@ class Card:
     DIAMONDS = "◆"
     HEARTS = "❤"
     SPADES = "♠"
-    #GLYPHS = ...
+    GLYPHS = {"♣":["🃑","🃒","🃓","🃔","🃕","🃖","🃗","🃘","🃙","🃚","🃛","🃝","🃞"],
+"◆":["🃁","🃂","🃃","🃄","🃅","🃆","🃇","🃈","🃉","🃊","🃋","🃍","🃎"],
+"❤":["🂱","🂲","🂳","🂴","🂵","🂶","🂷","🂸","🂹","🂺","🂻","🂽","🂾"],
+"♠":["🂡","🂢","🂣","🂤","🂥","🂦","🂧","🂨","🂩","🂪","🂫","🂭","🂮"]}
 
     def __init__(self, value: int, suit: str):
-        pass
+        self.value = value
+        self.suit = suit
+        if suit not in self.GLYPHS:
+            raise InvalidCardError(message=f"{repr(suit)} is not a supported suit")
+        if isinstance(value, int):
+            if not (1 <= value <= 13):
+                raise InvalidCardError(message=f"{repr(value)} is not a supported value")
+        self.value = value
+        
+    def __repr__(self):
+        """Devuelve el glifo de la carta"""
+        return self.GLYPHS[self.suit][self.value - 1]
 
 class Deck:
     def __init__(self):
@@ -17,3 +31,12 @@ class Deck:
 class Hand:          
     def __init__(self):
         ...
+
+class InvalidCardError(Exception):
+    def __init__(self, *, message: str = ""):
+        default_message = "🃏 Invalid card"
+        if not message:
+            self.message = default_message
+        else:
+            self.message = f"{default_message}: {message}"
+        super().__init__(self.message)
--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+
 class Game:
     def __init__(self, players: int = 2):
         self.players = players
