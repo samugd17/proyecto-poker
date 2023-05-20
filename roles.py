269f878ccbd2272080b693cc735a6073c51f278d
Samugd17 <samugd17@gmail.com>
2023-05-21 00:30:11 +0100

    Se corrigen errores

--- roles.py
+++ roles.py
@@ -1,14 +1,12 @@
 from __future__ import annotations
-
+import helpers, cards
 
 class Player:
     def __init__(self, name: str = "Neo"):
         self.name = name
-        self.cards = ...
+        self.cards = cards.Hand()
     ...
 class Dealer:
     def __init__(self, players: str):
-        ...
-class Hand:
-    def __init__(self):
-        ...
+        self.players = players
+        self.cards = cards.Deck()
