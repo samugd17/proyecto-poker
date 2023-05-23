d80134d87c0c9c7933e33996286c367a7126e43d
Samugd17 <samugd17@gmail.com>
2023-05-23 01:55:55 +0100

    Se añaden correcciones

--- roles.py
+++ roles.py
@@ -1,28 +1,25 @@
 from __future__ import annotations
 import helpers, cards
 
+
 class Player:
-    def __init__(self):
-        name = input("Introduce tu nombre: ").title()
-        self.name = name
-        self.player_cards = Dealer.give_player_cards()
-        self.table_cards = Dealer.take_common_cards()
-    
-    def choose_best_combination(self):
-        helpers.combinations(cards.Hand(), 5)
-        
+    def __init__(self, name: str):
+        self.name = name.title()
+
 class Dealer:
     def __init__(self, *players: Player):
         self.players = players
         self.cards = cards.Deck()
-    
+        self.player_cards = self.give_player_cards()
+        self.table_cards = self.take_common_cards()
+
     def take_common_cards(self):
         table_cards = ""
         for _ in range(5):
             new_card = str(self.cards.get_random_card())
             table_cards += new_card
         return table_cards
-    
+
     def give_player_cards(self):
         player_cards = ""
         for _ in range(2):
@@ -30,11 +27,12 @@ class Dealer:
             player_cards += new_card
         return player_cards
 
-player = Player()
-player1 = Player()
+
+player = Player("samu")
+player1 = Player("lolo")
 print(player.name)
 print(player1.name)
 
-dealer1 = Dealer(player,player1)
+dealer1 = Dealer(player, player1)
 print(dealer1.give_player_cards())
 print(dealer1.take_common_cards())
