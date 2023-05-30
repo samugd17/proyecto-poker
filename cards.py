828977889296d291400ef191d2d83dfce8e2b4b4
JaimeLGC <jaimeleongarcia123@gmail.com>
2023-05-30 19:19:15 +0100

    merge

--- cards.py
+++ cards.py
@@ -67,16 +67,10 @@ class Deck:
     def __getitem__(self, index: int) -> str:
         return self.cards[index]
 
-<<<<<<< HEAD
     def get_random_card(self):
         # -------------------------------------
         random_value = helpers.randint(1, Card.MAX_CARDS)
         # -------------------------------------
-=======
-    def get_random_card(self): 
-        random_value = helpers.randint(Card.MAX_CARDS)
-        Card.MAX_CARDS -= 1
->>>>>>> samuel
         return self.cards.pop(random_value)
     
     @property
