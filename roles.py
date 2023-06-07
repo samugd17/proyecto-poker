7ba6ee7e669498607d0d9ec022807ab10dd9ba64
Samugd17 <samugd17@gmail.com>
2023-06-07 18:44:57 +0100

    Se añade corrección a Dealer.py

--- roles.py
+++ roles.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 import helpers
-from cards import Deck
+from cards import Deck, Card
 
 
 class Player:
@@ -22,17 +22,17 @@ class Dealer:
         self.table_cards = self.take_common_cards()
 
     def take_common_cards(self):
-        table_cards = ""
+        table_cards = []
         for _ in range(5):
-            new_card = str(self.cards.get_random_card())
-            table_cards += new_card
+            new_card = self.cards.get_random_card()
+            table_cards.append(new_card)
         return table_cards
 
     def give_player_cards(self):
-        player_cards = ""
+        player_cards = []
         for _ in range(2):
-            new_card = str(self.cards.get_random_card())
-            player_cards += new_card
+            new_card = self.cards.get_random_card()
+            player_cards.append(new_card)
         return player_cards
 
     def __repr__(self) -> str:
