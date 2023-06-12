32099f33ef0309e535067ca25051b3299c76acdc
Samugd17 <samugd17@gmail.com>
2023-06-12 16:54:34 +0100

    Pasan más test

--- game.py
+++ game.py
@@ -22,15 +22,15 @@ def get_winner(players: list[Player], common_cards: list[Card], private_cards: l
 
 # game = Game()
 # game.start_game()
-# print(game.players[0].common_cards)
-# print(game.players[0].private_cards)
+# print(players[0].common_cards)
+# print(players[0].private_cards)
 # print(game.players[1].private_cards)
-# print(game.players[0].find_best_hand())
+#  print(game.players[0].find_best_hand())
 # print(game.players[1].find_best_hand())
 # print(game.players[0].get_cat_rank())
 # print(game.players[1].get_cat_rank())
-# players = [Player('Player 1'), Player('Player 2')]
-# common_cards = [Card('A◆'), Card('J❤'), Card('9◆'), Card('8❤'), Card('8♣')]
-# private_cards = [[Card('10♣'), Card('10❤')], [Card('5♣'), Card('2❤')]]
-# print(get_winner(players, common_cards, private_cards))
+players = [Player('Player 1'), Player('Player 2')]
+common_cards = [Card('A◆'), Card('J❤'), Card('8♣'), Card('7♣'), Card('6♠')]
+private_cards = [[Card('4♣'), Card('2❤')], [Card('9♠'), Card('2◆')]]
+print(get_winner(players, common_cards, private_cards))
 
--- roles.py
+++ roles.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 import helpers
 from cards import Deck, Hand
 
+
 class Player:
     def __init__(self, name: str):
         self.name = name.title()
@@ -10,38 +11,48 @@ class Player:
 
     def __repr__(self) -> str:
         return f"{self.private_cards}"
-    
+
     @property
     def show_game_cards(self) -> list:
         return self.private_cards + self.common_cards
-    
+
     def make_all_combinations(self) -> list:
         return list(helpers.combinations(self.show_game_cards, n=5))
-    
+
     def check_possible_hands(self):
-        cards= []
+        cards = []
         for combination in self.make_all_combinations():
-            sorted(combination)
             for card in combination:
                 cards.append(card)
-        card_combinations = [cards[i:i+5] for i in range(0, len(cards), 5)]
+        card_combinations = [cards[i : i + 5] for i in range(0, len(cards), 5)]
         return card_combinations
-    
+
     def find_best_hand(self):
         best_hand = [None, 0]
-        for combination in self.check_possible_hands():         
+        for combination in self.check_possible_hands():
             suits = [card.suit for card in combination]
             card_values = [card.cmp_value for card in combination]
             same_suit = len(set(suits)) == 1
 
             # Establecemos todas las combinaciones posibles
-            straight = sorted(card_values) == list(range(min(card_values), max(card_values) + 1))
+            straight = sorted(card_values) == list(
+                range(min(card_values), max(card_values) + 1)
+            )
             straight_flush = same_suit and straight
-            four_of_a_kind = any(card_values.count(value) == 4 for value in set(card_values))
-            full_house = (any(card_values.count(value) == 3 for value in set(card_values))and len(set(card_values)) == 2)
-            flush = any(suits.count(suit) == 5 for suit in set(suits))
-            three_of_a_kind = any(card_values.count(value) == 3 for value in set(card_values))
-            two_pairs = len(set(card_values)) == 3 and any(card_values.count(value) == 2 for value in set(card_values))
+            four_of_a_kind = any(
+                card_values.count(value) == 4 for value in set(card_values)
+            )
+            full_house = (
+                any(card_values.count(value) == 3 for value in set(card_values))
+                and len(set(card_values)) == 2
+            )
+            flush = same_suit
+            three_of_a_kind = any(
+                card_values.count(value) == 3 for value in set(card_values)
+            )
+            two_pairs = len(set(card_values)) == 3 and any(
+                card_values.count(value) == 2 for value in set(card_values)
+            )
             one_pair = len(set(card_values)) == 4
             high_card = len(set(card_values)) == 5
 
@@ -63,29 +74,40 @@ class Player:
                 actual_hand = [combination, Hand.ONE_PAIR]
             elif high_card:
                 actual_hand = [combination, Hand.HIGH_CARD]
-            
+
             if actual_hand[1] > best_hand[1]:
                 best_hand = actual_hand
+            elif actual_hand[1] == best_hand[1]:
+                n1 = sum(i.cmp_value for i in actual_hand[0])
+                n2 = sum(i.cmp_value for i in best_hand[0])
+                if n1 > n2:
+                    best_hand = actual_hand
         return best_hand
-    
+
     def get_cat_rank(self) -> str | tuple[str]:
         hand_cards, hand_cat = self.find_best_hand()
-        pairs_values = list(set(card.str_value for card in hand_cards if hand_cards.count(card) == 2))
-        three_of_a_kind_value = list(set(card.str_value for card in hand_cards if hand_cards.count(card) == 3))
-        four_of_a_kind_value = list(set(card.str_value for card in hand_cards if hand_cards.count(card) == 4))
-        
+        pairs_values = list(
+            set(card.str_value for card in hand_cards if hand_cards.count(card) == 2)
+        )
+        three_of_a_kind_value = list(
+            set(card.str_value for card in hand_cards if hand_cards.count(card) == 3)
+        )
+        four_of_a_kind_value = list(
+            set(card.str_value for card in hand_cards if hand_cards.count(card) == 4)
+        )
+
         if hand_cat == Hand.FOUR_OF_A_KIND:
             return four_of_a_kind_value[0]
-        elif hand_cat == Hand.FULL_HOUSE:
+        if hand_cat == Hand.FULL_HOUSE:
             return tuple(three_of_a_kind_value + pairs_values)
-        elif hand_cat == Hand.THREE_OF_A_KIND:
+        if hand_cat == Hand.THREE_OF_A_KIND:
             return three_of_a_kind_value[0]
-        elif hand_cat == Hand.TWO_PAIR:
+        if hand_cat == Hand.TWO_PAIR:
             formatted_pairs_values = sorted(pairs_values, reverse=True)
             return tuple(formatted_pairs_values)
-        elif hand_cat == Hand.ONE_PAIR:
+        if hand_cat == Hand.ONE_PAIR:
             return pairs_values[0]
-        high_card = (max(card for card in hand_cards))
+        high_card = max(card for card in hand_cards)
         return high_card.str_value
 
     def build_hand(self) -> Hand:
@@ -94,7 +116,8 @@ class Player:
         new_hand.cat = cat
         new_hand.cat_rank = self.get_cat_rank()
         return new_hand
-    
+
+
 class Dealer:
     def __init__(self, players: list[Player]):
         self.players = players
